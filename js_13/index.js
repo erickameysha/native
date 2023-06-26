4fecba7ff56799caa556f62c5e58fe720e6315b7
ekameysha <ekameyshaaa@gmail.com>
2023-06-26 17:27:54 +0300

    js_13

--- js_13/index.js
+++ js_13/index.js
@@ -1,37 +1,72 @@
-console.log('kfkkf')
+//this (call apply bind constructor)
 
-const men = {
-    _date: {},
+//!'use strict'{}
+//!'use strict'{}, undefined,null
+//1.GLobal scoupe
+//2.function (simple | arrow)
+//3 call apply bind
+//4 function constructor
+
+const f = () => {
+    console.log(this)
+}
+
+f()
+//OOP
+const man = {
+    _data: {},
     _counter: {
-        value: 0,
-        step: 2
+        value: 3,
+        step: 2,
+        // value: 0,
     },
-    increment(step) {
-        if (step === undefined) {
+    printCounterWithDelay(){
+setTimeout(()=>{
+    this.printCounter.bind(this)
+},1000)
+            // this.printCounter()
 
-            step = this._counter.step
-        }
-
-        this._counter.value += step
     },
-    // getter
-    getCount() {
+    printCounter(){
+        console.log(this._counter.value)
+    },
+    getCounter (){
+        console.log('this' + this)
         return this._counter.value
     },
-    // setter
-    setStep(newStep) {
-        return this._counter.step = newStep
+
+}
+const print = man.printCounterWithDelay
+print.bind(man)
+// console.log(man.getCounter())
+// const getCounter =man.getCounter
+// console.log(getCounter())
+const anyFnc = (counterable) => {
+    console.log(counterable.getCounter())
+}
+let anyProps={
+    getCounter:()=> {
+        return man.getCounter()
+    }}
+
+anyFnc(anyProps)
+
+// console.log(anyFunc(man))
+// man.increment()
+// // console.log(man)
+// man.getCounter()
+
+class Man {
+
+    constructor(props) {
+    this.props = props
+
+    }
+    render(){
+        console.log(this.props.age)
     }
 
-}
 
-// men.setStep(10)
-// men.increment()
-//
-// console.log(men.getCount())
-//
-
-const  anyFunc = (counterable)=>{
-    console.log(counterable.getCount())
 }
-anyFunc(men)
+const  men1 = new Man({age: 10})
+men1.render()
---
+++ promise/index.js
@@ -0,0 +1,14 @@
+
+
+
+ const obj ={
+    name:'',
+     say(message){
+        alert(message)
+     }
+ }
+ console.log(obj.name)
+ obj.name ='Dimich'
+
+ console.log(obj.name)
+ obj.say('dimich')
